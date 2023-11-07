e3bda3de8c9d967f6d803de62f2b21b3de425aa3
Salot Jay <salotjay@gmail.com>
2023-11-07 20:11:36 +0530

    fix: change UI

--- src/pages/[uid]/index.tsx
+++ src/pages/[uid]/index.tsx
@@ -5,6 +5,14 @@ import { db } from "@/firebase";
 import { useRouter } from "next/router";
 import axios from "axios";
 
+const inputStyle = {
+  width: "100%",
+  padding: "12px",
+  margin: "44px 0",
+  borderRadius: "5px",
+  border: "1px solid #ccc",
+};
+
 export default function Id() {
   const [email, setEmail] = useState("");
   const [isSubscribed, setIsSubscribed] = useState({
@@ -43,7 +51,7 @@ export default function Id() {
       .then(() => {
         setIsSubscribed({
           isModal: true,
-          error: "Successfully subscribed to question. Thank You!!!!",
+          error: "Successfully subscribed to question. Thank You",
         });
       })
       .catch((error) => {
@@ -54,68 +62,65 @@ export default function Id() {
       });
   };
 
+  console.log("isSubscribed", isSubscribed);
+
   return (
     <main>
       <div className={styles.main}>
-        <div style={{ color: "red", fontSize: "16px" }}>APP SCRIPT</div>
-        {!isSubscribed?.isModal ? (
-          <div
-            style={{
-              background: "white",
-              height: "400px",
-              width: "400px",
-              margin: "0px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <label style={{ marginBottom: "12px" }}>
-              Enter an email for subscribing question on App Script:
-            </label>
-            <input
-              type="text"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder="Email"
-              style={{
-                marginBottom: "12px",
-                width: "300px",
-                padding: "12px",
-              }}
-            />
-            <button
-              type="button"
-              onClick={handleSaveEmail}
-              style={{
-                width: "300px",
-                padding: "12px",
-                background: "blue",
-                color: "white",
-                border: "1px solid gray",
-                borderRadius: "30px",
-              }}
-            >
-              Save Email
-            </button>
-          </div>
-        ) : (
-          <div
-            style={{
-              background: "white",
-              height: "400px",
-              width: "400px",
-              margin: "0px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            Successfully subscribed to the question. Thank You !!!!!
-          </div>
-        )}
+        <div
+          style={{ fontSize: "32px", marginBottom: "20px" }}
+          className="gradient-text"
+        >
+          APP SCRIPT
+        </div>
+        <div
+          style={{
+            background: "#f0f0f0",
+            borderRadius: "10px",
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            padding: "20px",
+            width: "400px",
+            height: "400px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {isSubscribed?.isModal ? (
+            <div style={{ color: "green", fontSize: "20px" }}>
+              Successfully subscribed to the question. Thank You!!!{" "}
+            </div>
+          ) : (
+            <div>
+              <label>
+                Enter an email to subscribe to the question on App Script:
+              </label>
+              <input
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+                placeholder="Email"
+                style={inputStyle}
+              />
+              <button
+                type="button"
+                onClick={handleSaveEmail}
+                style={{
+                  width: "100%",
+                  padding: "12px",
+                  background: "blue",
+                  color: "white",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+              >
+                Save Email
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </main>
   );
